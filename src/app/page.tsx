b7b7e844ef8012fdf038d07c485c696d709f64d6
laizabsobral <laiza.b.sobral@accenture.com>
2023-11-07 16:31:10 -0300

    add seletores e estados

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,10 @@
 export default function Home() {
   return (
   <div className="p-8 bg-slate-900 text-slate-100 h-screen">
-    <h1 className="font-bold text-diego">Olá</h1>
+    <h1 className="font-bold text-5xl flex items-center gap before:w-0.5 before:h-8 before:bg-sky-500 before:flex">
+      Hello Manas
+    </h1>
+    <button disabled className="bg-sky-500 px-4 py-2 rounded-md font-medium mt-4 enabled:hover:bg-sky-600 disabled:opacity-60 disabled:cursor-not-allowed"> Sign in</button>
   </div>
   )
 }
