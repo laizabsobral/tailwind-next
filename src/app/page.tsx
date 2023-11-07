fd79e904906531684bb74737718c736901b187d7
laizabsobral <laiza.b.sobral@accenture.com>
2023-11-07 17:44:26 -0300

    upd breakpoints

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
   <div className="p-8 bg-slate-900 text-slate-100 h-screen">
-    <h1 className="font-bold text-5xl flex items-center gap before:w-0.5 before:h-8 before:bg-sky-500 before:flex">
+    <h1 className="font-bold text-3xl sm:text-5xl lg:text-6xl flex items-center gap before:w-0.5 before:h-8 before:bg-sky-500 before:flex">
       Hello Manas
     </h1>
     <button disabled className="bg-sky-500 px-4 py-2 rounded-md font-medium mt-4 enabled:hover:bg-sky-600 disabled:opacity-60 disabled:cursor-not-allowed"> Sign in</button>
